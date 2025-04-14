a36ec52c4283f1b11f3e1fc9fb196e0f0d6773dd
naveeng80 <naveen80.g@gmail.com>
2025-04-14 15:29:25 +0100

    edited4

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ app.get('/', (req, res) => {
 	res.send('Hello World! Divya')
 })
 
-app.listen(3000, () => console.log('App running on port 3000'))
+app.listen(3001, () => console.log('App running on port 3000'))
